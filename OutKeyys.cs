4b554a3e8fdba56f4abed919253bcdf6321a51d3
serenayekncx <77447115+serenayekncx@users.noreply.github.com>
2021-01-18 00:50:48 +0300

    Add files via upload

---
+++ OutKeyys.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MetotCalism
+{
+    class OutKeyys
+    {
+
+        static void Main(string[] args)
+        {
+            //out keyword
+
+            Console.WriteLine("---Outkys------");
+
+
+            int number;
+            int number2 = 35;
+
+            var result = Add(out int number1, number2);
+            Console.WriteLine(result);
+
+            static int Add(out int number1, int number2)
+            {
+                number1 = 28;
+                return number1 + number2;
+            }
+
+        }
+    }
+}
+
+
+
+
+
+
+    }
+
+}
