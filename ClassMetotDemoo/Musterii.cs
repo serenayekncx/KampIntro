b7651174c03ce84914f9f0bd4deec23fbb2c8954
serenayekncx <serenayyvz@gmail.com>
2021-01-20 02:29:10 +0300

    odev3

---
+++ ClassMetotDemoo/Musterii.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemoo
+{
+    class Musterii
+    {
+        public class Musteri
+        {
+            public string Id { get; set; }
+            public string Adi { get; set; }
+            public string Soyadi { get; set; }
+
+
+        }
+
+    }
+}
---
+++ ClassMetotDemoo/Musterimanagerr.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemoo
+{
+    class Musterimanagerr
+    {
+
+        public void Kaydet(Program.Musteri musteri)
+        {
+            Console.WriteLine("Sisteme Başarıyla Kayıt Oldunuz: " + musteri.MusteriAdi);
+
+
+        }
+
+
+    }
+}
