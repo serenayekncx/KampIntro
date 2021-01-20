ff7e8f80ef3f25b4af58b9d3e85cac3b501a006e
serenayekncx <serenayyvz@gmail.com>
2021-01-20 23:53:12 +0300

    20 Ocak

---
+++ Collections/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        //    string[] isimler = new string[] {"Murat","Serenay","Lukas","Faik" };
+        //    Console.WriteLine(isimler[0]);
+        //    Console.WriteLine(isimler[1]);
+        //    Console.WriteLine(isimler[2]);
+        //    Console.WriteLine(isimler[3]);
+
+        //    isimler = new string[5]; // --- yeni bi array atadın artık en alttaki cw'u 
+        //    //okumaz diziler genişlemez arrayin sınırları baştan belli.
+        //    isimler[4] = "İlker";
+        //    Console.WriteLine(isimler[4]);
+        //    Console.WriteLine(isimler[0]);
+
+
+            List<string> isimler2 = new List<string> { "Murat", "Serenay", "Lukas", "Faik" };
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            Console.WriteLine(isimler2[3]);
+            Console.WriteLine(isimler2[4]);
+            isimler2.Add("İlker");
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[4]);
+            
+            
+           
+        
+       
+}   }   }
---
+++ OOP1/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{      //snippet _ hazır tuşlar
+    class Product  //Entity (varlık)
+    {
+
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        public string ProductName { get; set; }
+        public double UnitPrice { get; set; }
+
+        public int UnitsInStock { get; set; }
+
+        //CRUD OPERATIONS CREATE READ UPDATE DELETE
+
+
+
+
+
+
+
+
+    }
+}
