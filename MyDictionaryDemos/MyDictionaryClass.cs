2c509931f14122a4b106d990296e6a9d54fdbd18
serenayekncx <serenayyvz@gmail.com>
2021-01-23 15:13:49 +0300

    DictionaryClass Odev 4. Hafta

---
+++ MyDictionaryDemos/MyDictionaryClass.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionaryDemos
+{
+    class MyDictionaryClass<T, K>
+    {
+        T[] ArrayKey;
+        K[] ArrayVal;
+
+        T[] TempKey;
+        K[] TempVal;
+
+        public MyDictionaryClass()
+        {
+
+            ArrayKey = new T[0];
+            ArrayVal = new K[0];
+
+        }
+        public void Add(T item, K value)
+        {
+            TempKey = ArrayKey;
+            TempVal = ArrayVal;
+
+            ArrayKey = new T[ArrayKey.Length + 1];
+            ArrayVal = new K[ArrayVal.Length + 1];
+
+            for (int i = 0; i < TempKey.Length; i++)
+            {
+
+                ArrayKey[i] = TempKey[i];
+            }
+            for (int i = 0; i < TempVal.Length; i++)
+            {
+                ArrayVal[i] = TempVal[i];
+            }
+
+                ArrayKey[ArrayKey.Length - 1] = item;
+                ArrayVal[ArrayVal.Length - 1] = value;
+
+                Output(ArrayKey, ArrayVal);
+        }
+        public void Output(T[] Arraykey, K[] ArrayVal)
+        {
+
+            for (int i = 0; i < Arraykey.Length; i++)
+            {
+
+                Console.WriteLine(" Ürün : " + Arraykey[i] +  " Fiyat : " + ArrayVal[i]);
+
+            }
+
+
+        }
+
+    }
+} 
---
+++ MyDictionaryDemos/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace MyDictionaryDemos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionaryClass<string, int> UrunFiyat1 = new MyDictionaryClass<string, int>();
+
+            UrunFiyat1.Add("Fosforlu Kalem", 15);
+            UrunFiyat1.Add("Silgi", 8);
+            UrunFiyat1.Add("Kalemlik", 35);
+            UrunFiyat1.Add("Çalışma Tahtası", 250);
+            UrunFiyat1.Add("Kareli Defter", 23);
+
+
+
+
+
+        }
+    }
+}
